868bf21277ad6337f3d751f2f833dd93678d6d11
Mateus Sobreira <matheus.sobreira.27@gmail.com>
2022-08-06 19:31:05 -0300

    fixing image priority

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,20 +4,21 @@ import Image from 'next/future/image';
 const Hero = () => {
   return (
     <section id="hero">
-      <div className="relative pt-16 md:pt-24 lg:-48">
-        <div className="absolute inset-0 bottom-8 md:bottom-24 xl:bottom-32 -z-10 bg-gradient-to-b from-amber-400 to-amber-600"></div>
+      <div className="lg:-48 relative pt-16 md:pt-24">
+        <div className="absolute inset-0 bottom-8 -z-10 bg-gradient-to-b from-amber-400 to-amber-600 md:bottom-24 xl:bottom-32"></div>
         <div className="container mx-auto grid grid-rows-1 place-items-end px-2">
           <Image
-            className="row-start-1 row-end-2 col-start-1 col-end-2 mx-auto"
+            className="col-start-1 col-end-2 row-start-1 row-end-2 mx-auto"
             width={1289}
             height={502}
+            priority={true}
             src="/couch.png"
             blurDataURL="/couch.png"
             placeholder="blur"
             alt="couch"
           />
           <Image
-            className="row-start-1 row-end-2 col-start-1 col-end-2 mx-auto w-40 sm:w-52 md:w-64 lg:w-72"
+            className="col-start-1 col-end-2 row-start-1 row-end-2 mx-auto w-40 sm:w-52 md:w-64 lg:w-72"
             width={326}
             height={643}
             src="/app.png"
